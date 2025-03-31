12b56f5fdb0bea590a47714ed9f4b8e101734540
Bekzat <baymukach@gmail.com>
2025-03-31 08:13:37 +0500

    Updated API functionality

--- handlers/book_handler.go
+++ handlers/book_handler.go
@@ -10,25 +10,11 @@ import (
 )
 
 var books = []models.Book{
-	{ID: 1, Title: "Abai joly", AuthorID: 1, CategoryID: 1, Price: 29.99},
+	{ID: 1, Title: "Go Programming", AuthorID: 1, CategoryID: 1, Price: 29.99},
 }
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
-	for i, book := range books {
-		for _, author := range authors {
-			if book.AuthorID == author.ID {
-				books[i].Author = &author
-			}
-		}
-		for _, category := range categories {
-			if book.CategoryID == category.ID {
-				books[i].Category = &category
-			}
-		}
-	}
-
 	json.NewEncoder(w).Encode(books)
 }
 
@@ -79,3 +65,85 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusNotFound)
 }
+
+//package handlers
+//
+//import (
+//	"encoding/json"
+//	"net/http"
+//	"strconv"
+//
+//	"bookstore/models"
+//	"github.com/gorilla/mux"
+//)
+//
+//var books = []models.Book{
+//	{ID: 1, Title: "Abai joly", AuthorID: 1, CategoryID: 1, Price: 29.99},
+//}
+//
+//func GetBooks(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//
+//	for i, book := range books {
+//		for _, author := range authors {
+//			if book.AuthorID == author.ID {
+//				books[i].Author = &author
+//			}
+//		}
+//		for _, category := range categories {
+//			if book.CategoryID == category.ID {
+//				books[i].Category = &category
+//			}
+//		}
+//	}
+//
+//	json.NewEncoder(w).Encode(books)
+//}
+//
+//func GetBook(w http.ResponseWriter, r *http.Request) {
+//	params := mux.Vars(r)
+//	id, _ := strconv.Atoi(params["id"])
+//	for _, book := range books {
+//		if book.ID == id {
+//			json.NewEncoder(w).Encode(book)
+//			return
+//		}
+//	}
+//	w.WriteHeader(http.StatusNotFound)
+//}
+//
+//func CreateBook(w http.ResponseWriter, r *http.Request) {
+//	var book models.Book
+//	json.NewDecoder(r.Body).Decode(&book)
+//	book.ID = len(books) + 1
+//	books = append(books, book)
+//	w.Header().Set("Content-Type", "application/json")
+//	json.NewEncoder(w).Encode(book)
+//}
+//
+//func UpdateBook(w http.ResponseWriter, r *http.Request) {
+//	params := mux.Vars(r)
+//	id, _ := strconv.Atoi(params["id"])
+//	for i, book := range books {
+//		if book.ID == id {
+//			json.NewDecoder(r.Body).Decode(&books[i])
+//			books[i].ID = id
+//			json.NewEncoder(w).Encode(books[i])
+//			return
+//		}
+//	}
+//	w.WriteHeader(http.StatusNotFound)
+//}
+//
+//func DeleteBook(w http.ResponseWriter, r *http.Request) {
+//	params := mux.Vars(r)
+//	id, _ := strconv.Atoi(params["id"])
+//	for i, book := range books {
+//		if book.ID == id {
+//			books = append(books[:i], books[i+1:]...)
+//			w.WriteHeader(http.StatusNoContent)
+//			return
+//		}
+//	}
+//	w.WriteHeader(http.StatusNotFound)
+//}
--- models/book.go
+++ models/book.go
@@ -1,11 +1,21 @@
 package models
 
 type Book struct {
-	ID         int       `json:"id"`
-	Title      string    `json:"title"`
-	AuthorID   int       `json:"author_id"`
-	CategoryID int       `json:"category_id"`
-	Price      float64   `json:"price"`
-	Author     *Author   `json:"author,omitempty"`   // Связь с автором
-	Category   *Category `json:"category,omitempty"` // Связь с категорией
+	ID         int     `json:"id"`
+	Title      string  `json:"title"`
+	AuthorID   int     `json:"author_id"`
+	CategoryID int     `json:"category_id"`
+	Price      float64 `json:"price"`
 }
+
+//package models
+//
+//type Book struct {
+//	ID         int       `json:"id"`
+//	Title      string    `json:"title"`
+//	AuthorID   int       `json:"author_id"`
+//	CategoryID int       `json:"category_id"`
+//	Price      float64   `json:"price"`
+//	Author     *Author   `json:"author,omitempty"`   // Связь с автором
+//	Category   *Category `json:"category,omitempty"` // Связь с категорией
+//}
